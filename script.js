292769c79732f83da784b93351c3a43bf66178ec
Ericchuks <you@example.com>
2022-08-04 17:44:42 +0100

    updated to include mobile

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-// let body = document.querySelector("body");
-// let modal = document.querySelector(".modal");
-// let menu = document.querySelector(".navLast");
+let body = document.querySelector("body");
+let modal = document.querySelector(".modal");
+let menu = document.querySelector(".navLast");
+let close = document.querySelector(".close");
+let section = document.querySelector(".bg")
+function hide(){
+    if(modal.style.display === "block"){
+        body.style.overflowY = "visible";
+        modal.style.display = "none";
+        // section.classlist.t(blurOnMenu)
+    }else{
+        modal.style.display = "block";
+        section.classlist.toggle("blurOnMenu")
+        body.style.overflowY = "hidden";
+    }
+}
 
-// function hide(){
-//     if(modal.style.display === "none"){
-//         body.style.overflowY = "hidden";
-//         modal.style.display = "block"
-//         console.log("Quad")
-//     }else{
-//         modal.style.display = "none";
-//         body.style.overflowY = "visible";
-//         console.log("Quad2")
-//     }
-// }
-
-// menu.addEventListener("click", hide)
+menu.addEventListener("click", hide)
+// modal.addEventListener("click", hide)
+close.addEventListener("click", hide)
