5771c7cd2d57196d2e6f60e30c3ec0a6d19b7a2a
Ericchuks <you@example.com>
2022-08-04 16:18:58 +0100

    updated

---
+++ script.js
@@ -0,0 +1,17 @@
+// let body = document.querySelector("body");
+// let modal = document.querySelector(".modal");
+// let menu = document.querySelector(".navLast");
+
+// function hide(){
+//     if(modal.style.display === "none"){
+//         body.style.overflowY = "hidden";
+//         modal.style.display = "block"
+//         console.log("Quad")
+//     }else{
+//         modal.style.display = "none";
+//         body.style.overflowY = "visible";
+//         console.log("Quad2")
+//     }
+// }
+
+// menu.addEventListener("click", hide)
