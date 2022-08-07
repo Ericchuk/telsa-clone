2c06a53f1d28b16dc318981b7e5ad29ddd8801ca
Ericchuks <you@example.com>
2022-08-07 11:24:52 +0100

    Merge branch 'master' of https://github.com/Ericchuk/telsa-clone

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 let body = document.querySelector("body");
 let modal = document.querySelector(".modal");
 let menu = document.querySelector(".navLast");
@@ -17,4 +18,25 @@ function hide(){
 
 menu.addEventListener("click", hide)
 // modal.addEventListener("click", hide)
+=======
+let body = document.querySelector("body");
+let modal = document.querySelector(".modal");
+let menu = document.querySelector(".navLast");
+let close = document.querySelector(".close");
+let section = document.querySelector(".bg")
+function hide(){
+    if(modal.style.display === "block"){
+        body.style.overflowY = "visible";
+        modal.style.display = "none";
+        // section.classlist.t(blurOnMenu)
+    }else{
+        modal.style.display = "block";
+        // section.classlist.toggle("blurOnMenu")
+        body.style.overflowY = "hidden";
+    }
+}
+
+menu.addEventListener("click", hide)
+// modal.addEventListener("click", hide)
+>>>>>>> c3a155ef472fa93a92ff5aa36969636b7ab30023
 close.addEventListener("click", hide)
